c080e244346da3f374f51ab32bc858a582ac605d
Sabina Psuj <sabinapsuj@gmail.com>
2025-01-11 20:23:40 +0100

    Add cards animation and minor styling changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react'
-import './App.css'
+import './App.scss'
 import flashcardsService  from './services/flashcards.service'
 import { FlashCard } from "./components/FlashCard"
 import { Flashcard as FlashCardType } from './types/Flashcard.type'
 
 function App() {
   const [cards, setCards] = useState<FlashCardType[]>([])
+  const [removingCard, setRemovingCard] = useState(false)
 
   useEffect(() => {
     if (!cards.length) {
@@ -14,22 +15,27 @@ function App() {
   }, [cards])
 
   const handleFlashCardClose = () => {
-    const newCards = [
-      ...cards.slice(1),
-      ...flashcardsService.getRandomFlashcards('js', 1),
-    ]
+    setRemovingCard(true)
+    setTimeout(() => {
+      const newCards = [
+        ...cards.slice(1),
+        ...flashcardsService.getRandomFlashcards('js', 1),
+      ]
 
-    setCards(newCards)
+      setCards(newCards)
+      setRemovingCard(false)
+    }, 400)
   }
 
   return (
-    <div>
-      {cards.map((card) => (
+    <div className={`app__cards${removingCard ? " app__cards--removing" : ""}`}>
+      {cards.map((card, index) => (
         <FlashCard
           key={card.id}
           flashcard={card}
           onClose={handleFlashCardClose}
           topic="js"
+          rotate={!!index}
         />
       ))}
     </div>
--- src/components/FlashCard.tsx
+++ src/components/FlashCard.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import type { Flashcard } from '../types/Flashcard.type'
 import './FlashCard.scss'
 
@@ -6,15 +6,24 @@ type FrashCardProps = {
   topic: string
   flashcard: Flashcard
   onClose: () => void
+  rotate: boolean
 }
 
-export function FlashCard({ flashcard, onClose, topic }: FrashCardProps) {
+export function FlashCard({ flashcard, onClose, topic, rotate }: FrashCardProps) {
   const [isFlipped, setIsFlipped] = useState(false)
+  const [rotation, setRotation] = useState(rotate ? (Math.random() * 6 + 5) * (Math.random() > 0.5 ? 1 : -1) : 0)
 
+  useEffect(() => {
+    if (!rotate) {
+      setRotation(0)
+    }
+  }, [rotate])
   return (
-    <div className={`flash-card  ${isFlipped ? "flash-card--flipped": ""} flash-card--topic_${topic}`}>
+    <div
+      className={`flash-card  ${isFlipped ? "flash-card--flipped": ""} flash-card--topic_${topic}`} 
+      style={{ ...(rotate ? {transform: `rotate(${rotation}deg)`} : {} )}}
+    >
       <div className={"flash-card__side flash-card__side--front"}>
-        <p>{flashcard.id}</p>
         <p>{flashcard.question}</p>
         <button onClick={() => setIsFlipped(true)}>Flip</button>
       </div>
